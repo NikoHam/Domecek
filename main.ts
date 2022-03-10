224f58295169134e78cb6bb1233557485629d348
NikoHam <58589799+NikoHam@users.noreply.github.com>
2022-03-10 12:26:47 +0100

    Aktualizovat main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,31 +1,35 @@
 input.onButtonPressed(Button.A, function () {
-    if (alarm == 0) {
-        alarm += 1
-    } else if (alarm == 2) {
-        alarm += 1
-    } else {
-        alarm = 0
+    if (hazard == 1) {
+        if (2 % alarm == 0) {
+            alarm += 1
+        } else {
+            alarm = 0
+        }
     }
 })
 input.onButtonPressed(Button.B, function () {
-    if (alarm == 1) {
-        alarm += 1
-    } else if (alarm == 3) {
-        alarm += 1
-    } else {
-        alarm = 0
+    if (hazard == 1) {
+        if (2 % alarm == 1) {
+            alarm += 1
+        } else {
+            alarm = 0
+        }
     }
 })
+let hazard = 0
 let alarm = 0
 alarm = 0
 let skříň = 0
+hazard = 0
 // ALARMY
 // 
 basic.forever(function () {
     if (smarthome.crashSensor()) {
         while (alarm != 4) {
             music.startMelody(music.builtInMelody(Melodies.Nyan), MelodyOptions.Once)
+            hazard = 0
         }
+        hazard = 1
     }
     if (smarthome.ReadTemperature(TMP36Type.TMP36_temperature_C, AnalogPin.P1) > 35) {
     	
